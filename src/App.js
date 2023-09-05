9860069804bdb20707091f9624dc75f79f208f69
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2023-09-05 16:10:09 +0530

    committing all changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-// import About from "./components/About";
-// import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -34,33 +34,35 @@ function App() {
   };
   return (
     <>
-      {/* <Router> */}
-      <Navbar title="TextUtilis" mode={mode} togglemode={togglemode} />
-      {/* <Navbar /> */}
-      <Alert alert={alert} />
+      <Router>
+        <Navbar title="TextUtilis" mode={mode} togglemode={togglemode} />
+        {/* <Navbar /> */}
+        <Alert alert={alert} />
 
-      <div className="container my-3">
-        <TextForm
+        <div className="container my-3">
+          {/* <TextForm
           heading="Enter the text to Analyze below"
           mode={mode}
           showAlert={showAlert}
-        />
-        {/* <Routes>
-            <Route exact path="/about" element={<About />} />
-            <Route
-              exact
-              path="/"
-              element={
-                <TextForm
-                  heading="Enter the text to Analyze below"
-                  mode={mode}
-                  showAlert={showAlert}
-                />
-              }
-            />
-          </Routes> */}
-      </div>
-      {/* </Router> */}
+        /> */}
+          {
+            <Routes>
+              <Route exact path="/about" element={<About />} />
+              <Route
+                exact
+                path="/"
+                element={
+                  <TextForm
+                    heading="Enter the text to Analyze below"
+                    mode={mode}
+                    showAlert={showAlert}
+                  />
+                }
+              />
+            </Routes>
+          }
+        </div>
+      </Router>
     </>
   );
 }
